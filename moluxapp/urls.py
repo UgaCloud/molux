9827390be70db171238118bc0a4405276d60e487
hacker <prycetreasure44@gmail.com>
2025-06-24 17:13:10 +0300

    molux2

--- moluxapp/urls.py
+++ moluxapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='home'),
-    path("/about", views.about, name='about'),
-    path("/gallery", views.gallery, name='gallery'),
-    path("/contact", views.contact, name='contact'),
+    path("about", views.about, name='about'),
+    path("gallery", views.gallery, name='gallery'),
+    path("contact", views.contact, name='contact'),
 ]
